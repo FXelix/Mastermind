1e1900f0a4378bfa68ca45bd5460b14d42d43f37
FXelix <felix.scheling@web.de>
2017-06-21 20:50:11 +0200

    Initial commit

---
+++ mastermind.py
@@ -0,0 +1,26 @@
+
+"""
+This is a simple version of the game mastermind.
+The goal is to guess the 4 random colors.
+
+Rules:
+- The colors: red, blue, green, yellow, orange
+- The code has only unique colors, no duplicates.
+- Every position must have a color.
+- After guessing the Codemaster gives hints.
+    - black pin: Correct color and correct position.
+    - white pin: Correct color but wrong position.
+
+"""
+
+# TODO: Create game enviroment and variables
+
+
+# TODO: Choose a random sample of colors
+
+
+#  TODO: After a guess, give feedback
+
+
+# TODO: If guess is correct, ask to play again
+
