695a85ba18558f613764265e26b83b79dcda09f2
FXelix <felix.scheling@web.de>
2017-06-22 21:24:43 +0200

    Update - updated feedback to not give unwanted hint of positions. - minor text changes

--- mastermind.py
+++ mastermind.py
@@ -18,39 +18,39 @@ Rules:
 def mastermind():
     tries = 0
 
-    colors = {"r": "red",
-              "b": "blue",
-              "g": "green",
-              "y": "yellow",
-              "o": "orange"}
+    colors = {"r": "Red",
+              "b": "Blue",
+              "g": "Green",
+              "y": "Yellow",
+              "o": "Orange"}
     codemaster = random.sample(list(colors.keys()), 4)
 
     while tries <= 10:
         try:
-            guess = list(input("Enter 4 colors as a guess: "))
+            guess = list(input("Your guess: "))
             if len(set(guess)) != 4:
                 raise ValueError
             if not set(guess) == set(i for i in colors.keys() if i in guess):
                 raise ValueError
         except ValueError:
-            print("Invalid input. Example input: gbyo")
+            print("Invalid input. \n")
             continue
-
+        print("-"*35)
         print("You chose: 1: {}, 2: {}, 3: {}, 4: {}".format(colors[guess[0]], colors[guess[1]], colors[guess[2]], colors[guess[3]]))
 
         if guess == codemaster:
             print("You cracked the code! Congratulations!")
             break
         else:
+            black_pin = 0
+            white_pin = 0
             for color in range(4):
                 if guess[color] == codemaster[color]:
-                    print("One black pin.")
+                    black_pin += 1
                 elif guess[color] != codemaster[color] and guess[color] in codemaster:
-                    print("One white pin.")
+                    white_pin += 1
+            print("{} black pins. {} white pins. \n".format(black_pin, white_pin)+ "-"*25)
         tries += 1
-
-
-#  TODO: Print pretty feedback
 
 
 # TODO: If guess is correct, ask to play again
